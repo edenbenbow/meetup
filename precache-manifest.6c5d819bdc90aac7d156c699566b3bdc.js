5a5e43ed40ca600a7df4fc721774e49ad61fc337
edenbenbow <edenbenbow@users.noreply.github.com>
2020-04-26 11:30:00 -0700

    OfflineAlert pt. 2

---
+++ precache-manifest.6c5d819bdc90aac7d156c699566b3bdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a43830833833b2e68548bedf28d37ec",
+    "url": "/meetup/index.html"
+  },
+  {
+    "revision": "8db737fa26cd6286bc5f",
+    "url": "/meetup/static/css/main.f627eb33.chunk.css"
+  },
+  {
+    "revision": "217be1cf4ddfc41628e9",
+    "url": "/meetup/static/js/2.da247d7e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/meetup/static/js/2.da247d7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db737fa26cd6286bc5f",
+    "url": "/meetup/static/js/main.6055e8cf.chunk.js"
+  },
+  {
+    "revision": "85fbb69b7d346ae6f740",
+    "url": "/meetup/static/js/runtime-main.878c62fa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meetup/precache-manifest.1754bec91484be3f05e117a9dcee47c7.js"
+  "/meetup/precache-manifest.6c5d819bdc90aac7d156c699566b3bdc.js"
 );
 
 self.addEventListener('message', (event) => {
